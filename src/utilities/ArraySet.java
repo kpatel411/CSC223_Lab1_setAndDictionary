5a9df49255ab1c0b1cd0d6f199cd897c8e678d30
Khushi Patel <khushipatel411@gmail.com>
2023-01-10 15:10:36 -0500

    Step 1

--- src/utilities/ArraySet.java
+++ src/utilities/ArraySet.java
@@ -1,8 +1,9 @@
 package utilities;
 
-public class ArraySet
+public class ArraySet <E> implements List<E>, Set<E>
 {
-
+	protected ArrayList<E> _list;
+	
 	public ArraySet()
 	{
 		// TODO Auto-generated constructor stub
