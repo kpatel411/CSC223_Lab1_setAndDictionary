e452dc86cc3fa629d6d80379e6b8a9e2248e62ce
Khushi Patel <khushipatel411@gmail.com>
2023-01-19 19:40:33 -0500

    more tests

--- test/utilities/ArraySetTest.java
+++ test/utilities/ArraySetTest.java
@@ -24,7 +24,10 @@ class ArraySetTest
 		//add strings to new test object
 		test1.add(1);
 		test1.add(2);
-		assertEquals(2, test1.size());
+		test1.add(3);
+		test1.add(4);
+		test1.add(5);
+		assertEquals(5, test1.size());
 		
 		//try testing boolean isEmpty
 		ArraySet <Integer> test2 = new ArraySet<Integer>();
@@ -38,19 +41,35 @@ class ArraySetTest
 	{
 		//adds objects to list
 		ArraySet <Integer> test1 = new ArraySet<Integer>();
+		ArraySet <Integer> test2 = new ArraySet<Integer>();
 		test1.add(80);
 		test1.add(75);
 		test1.add(70);
-		assertEquals(3, test1.size());
+		test2.add(80);
+		test2.add(75);
+		test2.add(70);
+		assertEquals(test1.size(), test2.size());
+		
+		//testing with duplicates
+		ArraySet <Integer> testDupe = new ArraySet<Integer>();
+		ArraySet <Integer> testDupe2 = new ArraySet<Integer>();
+		testDupe.add(80);
+		testDupe.add(75);
+		testDupe.add(70);
+		testDupe2.add(80);
+		testDupe2.add(75);
+		testDupe2.add(70);
+		testDupe.addAll(testDupe2);
+		assertEquals(testDupe.size(), testDupe.size());
 		
 		//testing with empty
-		ArraySet <Integer> test2 = new ArraySet<Integer>();
-		assertEquals(0, test2.size());
+		ArraySet <Integer> testEmpty = new ArraySet<Integer>();
+		assertEquals(0, testEmpty.size());
 		
 		//testing with a null value
-		ArraySet <Integer> test3 = new ArraySet<Integer>();
-		test3.add(null);
-		assertEquals(1, test3.size());
+		ArraySet <Integer> testNull = new ArraySet<Integer>();
+		testNull.add(null);
+		assertEquals(1, testNull.size());
 		
 	}
 
@@ -69,6 +88,19 @@ class ArraySetTest
 		test1.addAll(test2);
 		assertEquals(2, test1.size());
 		
+		//Testing with duplicates 
+		ArraySet <String> testDupe = new ArraySet<String>();
+		ArraySet <String> testDupe2 = new ArraySet<String>();
+		testDupe.add("hello");
+		testDupe.add("hi");
+		testDupe2.add("hello");
+		testDupe2.add("hi");
+		assertEquals(testDupe.size(), testDupe2.size());
+		
+		//test with empty
+		ArraySet <String> testEmpty = new ArraySet<String>();
+		assertEquals(0,testEmpty.size());
+		
 		//test with null values
 		ArraySet <String> testA = new ArraySet<String>();
 		//add values to arraylist
@@ -80,9 +112,6 @@ class ArraySetTest
 		testA.addAll(testB);
 		assertEquals(2, testA.size());
 		
-		//test with empty
-		ArraySet <String> testEmpty = new ArraySet<String>();
-		assertEquals(0,testEmpty.size());
 		
 	}
 	
@@ -99,6 +128,15 @@ class ArraySetTest
 		//addAll to combine
 		test1.addAll(test2);
 		assertEquals(2, test1.size());
+		
+		//test with duplicates
+		ArraySet <Integer> testDupe = new ArraySet<Integer>();
+		ArraySet <Integer> testDupe2 = new ArraySet<Integer>();
+		testDupe.add(35);
+		testDupe.add(80);
+		testDupe2.add(35);
+		testDupe2.add(80);
+		assertEquals(testDupe.size(), testDupe2.size());
 		
 		//test with null values
 		ArraySet <Integer> testA = new ArraySet<Integer>();
@@ -133,7 +171,6 @@ class ArraySetTest
 		test2.add("ghi");
 		//retain all matching values 
 		test2.retainAll(test1);
-		//assertEquals(2, test1.size());
 		assertEquals(2, test2.size());
 		
 		//test retainAll (collection c) if values do not exist
